e5fd74aa581e57bd8ac6dbe29c687f4f8d1cad4e
hyj <huoyujia@sspaas.com>
2024-09-09 15:09:22 +0800

    feat：Set RBAC permissions for NameService

--- internal/controller/nameservice_controller.go
+++ internal/controller/nameservice_controller.go
@@ -53,6 +53,7 @@ type NameServiceReconciler struct {
 // +kubebuilder:rbac:groups=apps.hyj.cn,resources=nameservices,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps.hyj.cn,resources=nameservices/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps.hyj.cn,resources=nameservices/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps.hyj.cn,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
